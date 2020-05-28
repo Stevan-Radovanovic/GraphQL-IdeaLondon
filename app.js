fca27d0994583a15ab85bbb722d578ba644952f4
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-05-28 22:42:06 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let graphqlHTTP = require('express-graphql');
+let { buildSchema } = require('graphql');
+
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+let root = { hello: () => 'Hello world!' };
+
+let app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
