2ef2d101795210933824b48bb4fd7b2e0026708b
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-05-29 14:50:42 +0200

    Added mongoose

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ let express = require('express');
 let graphqlHTTP = require('express-graphql');
 let schema = require("./schema");
 let resolver = require("./resolvers");
-
+let mongoose = require('mongoose');
 
 let app = express();
+
+mongoose
+  .connect(
+    'mongodb+srv://stevan:Stevan.1@cluster0-yxv9s.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('Connected to the database!');
+  })
+  .catch(() => {
+    console.log('Connection to the database failed');
+  });
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
